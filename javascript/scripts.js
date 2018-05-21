6c408fb75d6370dc0d096f3e11b3d6b077742f15
Lukasz Kujawa <lukasz.kujawa@interia.pl>
2018-05-21 21:49:38 +0200

    create files, task

---
+++ javascript/scripts.js
@@ -0,0 +1,30 @@
+function Phone(brand, price, color) {
+  this.brand = brand;
+  this.price = price;
+  this.color = color;
+}
+
+Phone.prototype.printInfo = function() {
+  console.log('Marka telefonu ' + this.brand + ', kolor ' + this.color + ' cena wynosi ' + this.price + ' zł.');
+}
+
+const iPhone8plus = new Phone('Apple', 3450, 'srebrny');
+const SamsungS9 = new Phone('Samsung', 2200, 'złoty');
+const HuaweiP20Pro = new Phone('Huawei', 3400 , 'brąz');
+
+iPhone8plus.printInfo();
+SamsungS9.printInfo();
+HuaweiP20Pro.printInfo();
+
+Phone.prototype.winterSale = function () {
+  let date;
+  for(date = 0; date <= 31; date++) {
+    if(date >= 20 && date < 21) {
+      console.log('Wiosenna wyprzedaż ' + this.brand + ', kolor ' + this.color + ' cena wynosi ' + this.price * 0.8 + ' zł.');
+    }
+  }
+}
+
+iPhone8plus.winterSale();
+SamsungS9.winterSale();
+HuaweiP20Pro.winterSale();
